81a6e66188eaea99897d9f88b6e7c74dbf7c436c
unknown <gtluu912@gmail.com>
2022-10-11 13:51:36 -0700

    add idbac installation instructions for ubuntu

---
+++ idbac_installation_instructions/data/install_idbac.R
@@ -0,0 +1,6 @@
+install.packages('devtools')
+
+install.packages('https://cran.r-project.org/src/contrib/Archive/MALDIquant/MALDIquant_1.19.3.tar.gz', repos=NULL, type='source')
+install.packages('https://cran.r-project.org/src/contrib/MALDIquantForeign_0.13.tar.gz', repos=NULL, type='source')
+#devtools::install_github("chasemc/IDBacApp@*release")
+devtools::install_github("chasemc/IDBacApp@1.0.100")
---
+++ idbac_installation_instructions/data/install_ubuntu_dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install libssl-dev
+sudo apt-get install libxml2-dev
+sudo apt-get install liblz4-dev
+sudo apt-get install libnetcdf-dev
+sudo apt-get install libfontconfig1-dev
+sudo apt-get install libblas-dev liblapack-dev libarpack2-dev
+#sudo apt-get install gcc-fortran
+sudo apt-get install libgfortran4
