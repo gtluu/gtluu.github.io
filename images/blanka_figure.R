9b0da2352b40c6a1505a1bd35ebc6c3ea6fd7dfc
unknown <gtluu912@gmail.com>
2023-03-13 12:53:35 -0700

    remove blanka images

--- images/blanka_figure.R
+++
@@ -1,9 +0,0 @@
-library(MSnbase)
-
-setwd('E:/Code/root/root/images')
-
-blanka <- readMgfData('blanka_msms.mgf')
-blanka_media <- readMgfData('blanka_media_msms.mgf')
-
-MSnbase::plot(blanka, full=TRUE, w1=2)
-MSnbase::plot(blanka_media, full=TRUE, w1=2)
